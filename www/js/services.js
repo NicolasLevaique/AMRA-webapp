b4e8636c0983c0cdd168faad7d4fd91fa57fe0ab
Nicolas <nicolas.levaique@ubleam.com>
2014-10-09 19:44:23 +0200

    App skeleton. Some UI and communication with backend

---
+++ www/js/services.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Nicolas on 06/10/2014.
+ */
+
+angular.module('starter.services', [])
+
+  .service('PathsService', ['$http', '$q', function($http, $q) {
+    return {
+      getPath: function (pathId) {
+        var deferred = $q.defer();
+        //ed294220-4d3f-11e4-916c-0800200c9a66
+        $http.get('http://localhost:8200/api/paths/' + pathId).success(function (path) {
+          deferred.resolve(path);
+        }).error(function (err, status) {
+          deferred.reject(status);
+        });
+        return deferred.promise;
+      }
+    }
+  }]);
