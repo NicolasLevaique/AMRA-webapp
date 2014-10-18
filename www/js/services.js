5df029dd26d711498fdd5e99487f187664df8079
Nicolas <nicolas.levaique@ubleam.com>
2014-10-18 13:30:19 +0200

    new service to manage (poorly) environment

--- www/js/services.js
+++ www/js/services.js
@@ -4,12 +4,21 @@
 
 angular.module('starter.services', [])
 
-  .service('PathsService', ['$http', '$q', function($http, $q) {
+  .service('Environment', [function() {
+    return {
+      //if dev
+      backend: 'http://localhost:8200/api/'
+      // if prod
+//      backend: 'http://etud.insa-toulouse.fr/~levaique/amra/'
+    }
+  }])
+
+  .service('PathsService', ['$http', '$q', 'Environment', function($http, $q, Environment) {
     return {
       getPath: function (pathId) {
         var deferred = $q.defer();
         //ed294220-4d3f-11e4-916c-0800200c9a66
-        $http.get('http://localhost:8200/api/paths/' + pathId).success(function (path) {
+        $http.get(Environment.backend + 'paths/' + pathId).success(function (path) {
           deferred.resolve(path);
         }).error(function (err, status) {
           deferred.reject(status);
